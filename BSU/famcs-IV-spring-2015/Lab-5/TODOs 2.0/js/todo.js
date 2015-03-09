db5ae012bb434fd874b64d04a3e2120765a55401
Pavel Husakouski <phusakouski@exadel.com>
2015-03-09 23:50:45 +0300

    Lab-5 Simplification

--- BSU/famcs-IV-spring-2015/Lab-5/TODOs 2.0/js/todo.js
+++ BSU/famcs-IV-spring-2015/Lab-5/TODOs 2.0/js/todo.js
@@ -1,40 +1,38 @@
 var uniqueId = function() {
 	var date = Date.now();
-
 	var random = Math.random() * Math.random();
 
-	return Math.floor(date * random);
+	return Math.floor(date * random).toString();
 };
 
 var theTask = function(text, done) {
 	return {
 		description:text,
-		done:done,
+		done: !!done,
 		id: uniqueId()
 	};
 };
 
-var taskList = null;
+var taskList = [];
 
 function run(){
-	taskList = restore() || [ theTask('Сделать разметку', true),
-	theTask('Выучить JavaScript', true),
-	theTask('Написать чат !', false)
-	];
-
 	var appContainer = document.getElementsByClassName('todos')[0];
 
 	appContainer.addEventListener('click', delegateEvent);
 	appContainer.addEventListener('change', delegateEvent);
 
-	createAllTasks();
+	var allTasks = restore() || [ theTask('Сделать разметку', true),
+			theTask('Выучить JavaScript', true),
+			theTask('Написать чат !')
+		];
+
+	createAllTasks(allTasks);
 	updateCounter();
 }
 
-function createAllTasks() {
-	for(var i = 0; i < taskList.length; i++) {
-		addTodo(taskList[i]);
-	}
+function createAllTasks(allTasks) {
+	for(var i = 0; i < allTasks.length; i++)
+		addTodo(allTasks[i]);
 }
 
 function delegateEvent(evtObj) {
@@ -49,35 +47,34 @@ function delegateEvent(evtObj) {
 
 function onAddButtonClick(){
 	var todoText = document.getElementById('todoText');
+	var newTask = theTask(todoText.value);
 
 	if(todoText.value == '')
 		return;
 
-	var newTask = theTask(todoText.value, false);
-
-	taskList.push(newTask);
 	addTodo(newTask);
 	todoText.value = '';
 	updateCounter();
-	store();
+	store(taskList);
 } 
 
 function onToggleItem(divItem) {
-	var id = getIdOfTask(divItem);
+	var id = divItem.attributes['data-task-id'].value;
 
 	for(var i = 0; i < taskList.length; i++) {
 		if(taskList[i].id != id)
 			continue;
 
-		toggle(divItem, taskList[i]);
-		store();
+		toggle(taskList[i]);
+		updateItem(divItem, taskList[i]);
+		store(taskList);
+
 		return;
 	}
 }
 
-function toggle(divItem, task) {
+function toggle(task) {
 	task.done = !task.done;
-	updateItem(divItem, task);
 }
 
 function updateItem(divItem, task){
@@ -89,20 +86,15 @@ function updateItem(divItem, task){
 		divItem.firstChild.checked = false;
 	}
 
-	divItem.setAttribute('data-task-id', task.id.toString());
+	divItem.setAttribute('data-task-id', task.id);
 	divItem.lastChild.textContent = task.description;
-}
-
-function getIdOfTask(divItem) {
-	var dataTaskId = divItem.attributes['data-task-id'].value;
-
-	return parseInt(dataTaskId);
 }
 
 function addTodo(task) {
 	var item = createItem(task);
 	var items = document.getElementsByClassName('items')[0];
 
+	taskList.push(task);
 	items.appendChild(item);
 }
 
@@ -124,13 +116,13 @@ function updateCounter(){
 	counter.innerText = items.children.length.toString();
 }
 
-function store() {
+function store(listToSave) {
 	if(typeof(Storage) == "undefined") {
 		alert('localStorage is not accessible');
 		return;
 	}
 
-	localStorage.setItem("TODOs taskList", JSON.stringify(taskList));
+	localStorage.setItem("TODOs taskList", JSON.stringify(listToSave));
 }
 
 function restore() {
@@ -141,8 +133,5 @@ function restore() {
 
 	var item = localStorage.getItem("TODOs taskList");
 
-	if(item == null)
-		return null;
-
-	return JSON.parse(item);
+	return item && JSON.parse(item);
 }
