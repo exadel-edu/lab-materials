41fe2e0054f7523b7a8bd5f0127ae4fef495b2b9
Pavel Husakouski <phusakouski@exadel.com>
2015-03-30 20:15:12 +0300

    PUT, code style, 'done' fix

--- BSU/famcs-IV-spring-2015/Lab-8/TODOs Server/MessageExchange.java
+++ BSU/famcs-IV-spring-2015/Lab-8/TODOs Server/MessageExchange.java
@@ -11,6 +11,7 @@ public class MessageExchange {
 
     public String getToken(int index) {
         Integer number = index * 8 + 11;
+
         return "TN" + number + "EN";
     }
 
@@ -21,20 +22,24 @@ public class MessageExchange {
     public String getServerResponse(List<Task> tasks, int index) {
         List<Task> chunk = tasks.subList(index, tasks.size());
         JSONObject jsonObject = new JSONObject();
+
         jsonObject.put("tasks", chunk);
         jsonObject.put("token", getToken(tasks.size()));
+
         return jsonObject.toJSONString();
     }
 
-    public Task getClientMessage(InputStream inputStream) throws ParseException {
+    public Task getClientMessage(InputStream inputStream) throws Exception {
         JSONObject json = getJSONObject(inputStreamToString(inputStream));
 
-        return new Task((String)json.get("id"), (String)json.get("description"), "true".equals(json.get("done")) );
+        return new Task((String)json.get("id"), (String)json.get("description"), (Boolean)json.get("done") );
     }
 
     public String getErrorMessage(String text) {
         JSONObject jsonObject = new JSONObject();
+
         jsonObject.put("error", text);
+
         return jsonObject.toJSONString();
     }
 
@@ -42,20 +47,16 @@ public class MessageExchange {
         return (JSONObject) jsonParser.parse(json.trim());
     }
 
-    public String inputStreamToString(InputStream in) {
+    public String inputStreamToString(InputStream in) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         byte[] buffer = new byte[1024];
         int length = 0;
-        try {
-            while ((length = in.read(buffer)) != -1) {
-                baos.write(buffer, 0, length);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+
+        while ((length = in.read(buffer)) != -1) {
+            baos.write(buffer, 0, length);
         }
 
         System.out.println("Input stream " + new String(baos.toByteArray()));
-
         return new String(baos.toByteArray());
     }
 }
--- BSU/famcs-IV-spring-2015/Lab-8/TODOs Server/Server.java
+++ BSU/famcs-IV-spring-2015/Lab-8/TODOs Server/Server.java
@@ -15,36 +15,40 @@ import java.util.Map;
 import java.util.Arrays;
 
 public class Server implements HttpHandler {
-    private static Task[] predefinedTasks = { new Task("1","Create markup", true), new Task("2","Learn JavaScript", true), new Task("3","Write The Chat !", false) };
+    private static Task[] predefinedTasks = { 
+        new Task("1","Create markup", true), 
+        new Task("2","Learn JavaScript", true), 
+        new Task("3","Write The Chat !", false) 
+    };
     private List<Task> taskList = new ArrayList<Task>(Arrays.asList(predefinedTasks));
-
     private MessageExchange messageExchange = new MessageExchange();
 
     public static void main(String[] args) {
-        if (args.length != 1)
+        if (args.length != 1) {
             System.out.println("Usage: java Server port");
-        else {
-            try {
-                System.out.println("Server is starting...");
-                Integer port = Integer.parseInt(args[0]);
-                HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-                System.out.println("Server started.");
-                String serverHost = InetAddress.getLocalHost().getHostAddress();
-                System.out.println("Get list of tasks: GET http://" + serverHost + ":" + port + "/todos?token={token}");
-                System.out.println("Add new task: POST http://" + serverHost + ":" + port + "/todos");
-                System.out.println("Edit task: PUT http://" + serverHost + ":" + port + "/todos?id={id}");
+            return;
+        }
+        try {
+            System.out.println("Server is starting...");
+            Integer port = Integer.parseInt(args[0]);
+            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+            System.out.println("Server started.");
+            String serverHost = InetAddress.getLocalHost().getHostAddress();
+            System.out.println("Get list of tasks: GET http://" + serverHost + ":" + port + "/todos?token={token}");
+            System.out.println("Add new task: POST http://" + serverHost + ":" + port + "/todos");
+            System.out.println("Edit task: PUT http://" + serverHost + ":" + port + "/todos?id={id}");
 
-                server.createContext("/todos", new Server());
-                server.setExecutor(null);
-                server.start();
-            } catch (IOException e) {
-                System.out.println("Error creating http server: " + e);
-            }
+            server.createContext("/todos", new Server());
+            server.setExecutor(null);
+            server.start();
+        } catch (IOException e) {
+            System.out.println("Error creating http server");
+            e.printStackTrace();
         }
     }
 
     @Override
-    public void handle(HttpExchange httpExchange) throws IOException {
+    public void handle(HttpExchange httpExchange) {
         String response = "";
 
         try {
@@ -56,78 +60,93 @@ public class Server implements HttpHandler {
                 response = doGet(httpExchange);
             } else if ("POST".equals(httpExchange.getRequestMethod())) {
                 doPost(httpExchange);
+            } else if ("PUT".equals(httpExchange.getRequestMethod())) {
+                doPut(httpExchange);
             } else if ("OPTIONS".equals(httpExchange.getRequestMethod())) {
                 response = "";
             } else {
                 throw new Exception("Unsupported http method: " + httpExchange.getRequestMethod());
             }
 
-            System.out.println("End request " + httpExchange.getRequestMethod());
-
             sendResponse(httpExchange, response);
+            System.out.println("Response sent, size " + response.length());
+            System.out.println("End request " + httpExchange.getRequestMethod());
+            return;
         } catch (Exception e) {
             response = messageExchange.getErrorMessage(e.getMessage());
+            e.printStackTrace();
+        } 
 
+        try{
             sendResponse(httpExchange, response);
+        } catch(Exception e) {
+            System.out.println("Unable to send response !");
         }
-
-        System.out.println("Response sent, size " + response.length());
     }
 
-    private String doGet(HttpExchange httpExchange) {
+    private String doGet(HttpExchange httpExchange) throws Exception {
         String query = httpExchange.getRequestURI().getQuery();
 
         if (query != null) {
             Map<String, String> map = queryToMap(query);
             String token = map.get("token");
-
             System.out.println("Token " + token);            
 
             if (token != null && !"".equals(token)) {
                 int index = messageExchange.getIndex(token);
-
                 System.out.println("Index " + index);            
-
                 return messageExchange.getServerResponse(taskList, index);
-            } else {
-                return "Token query parameter is absent in url: " + query;
-            }
+            } 
+            throw new Exception("Token query parameter is absent in url: " + query);
         }
-        return  "Absent query in url";
+        throw new Exception("Absent query in url");
     }
 
     private void doPost(HttpExchange httpExchange) throws Exception {
-        try {
-            Task task = messageExchange.getClientMessage(httpExchange.getRequestBody());
-            System.out.println("New task from User : " + task.toString() );
-            taskList.add(task);
-        } catch (Exception e) {
-            System.err.println("Invalid task: " + httpExchange.getRequestBody() + " " + e.getMessage());
+        Task task = messageExchange.getClientMessage(httpExchange.getRequestBody());
+        System.out.println("New task from User : " + task.toString() );
 
-            throw e;
-        }
+        taskList.add(task);
     }
 
-    private void sendResponse(HttpExchange httpExchange, String response) {
-        try {
-            byte[] bytes = response.getBytes();
-            Headers headers = httpExchange.getResponseHeaders();
-            headers.add("Access-Control-Allow-Origin","*");
-            if("OPTIONS".equals(httpExchange.getRequestMethod())) {
-                headers.add("Access-Control-Allow-Methods","PUT, DELETE, POST, GET, OPTIONS");
+    private void doPut(HttpExchange httpExchange) throws Exception {
+        Task task = messageExchange.getClientMessage(httpExchange.getRequestBody());
+        System.out.println("Update task from User : " + task.toString() );
+
+        for(Task item : taskList) {
+            if(task.getId().equals(item.getId())) {
+                item.setDescription(task.getDescription());
+                item.setDone(task.getDone());
+                return;
             }
-            httpExchange.sendResponseHeaders(200, bytes.length);
-            OutputStream os = httpExchange.getResponseBody();
-            os.write( bytes);
-            os.flush();
-            os.close();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
+        throw new Exception("Invalid task id " + task.getId());
+    }
+
+    private void sendResponse(HttpExchange httpExchange, String response) throws IOException {
+        byte[] bytes = response.getBytes();
+        Headers headers = httpExchange.getResponseHeaders();
+
+        headers.add("Access-Control-Allow-Origin","*");
+        if("OPTIONS".equals(httpExchange.getRequestMethod())) {
+            headers.add("Access-Control-Allow-Methods","PUT, DELETE, POST, GET, OPTIONS");
+        }
+        httpExchange.sendResponseHeaders(200, bytes.length);
+        writeBody(httpExchange, bytes);
+    }
+
+    private void writeBody(HttpExchange httpExchange, byte[] bytes) throws IOException {
+        OutputStream os = httpExchange.getResponseBody();
+
+        os.write( bytes);
+        os.flush();
+        os.close();
     }
 
     private Map<String, String> queryToMap(String query) {
         Map<String, String> result = new HashMap<String, String>();
+
         for (String param : query.split("&")) {
             String pair[] = param.split("=");
             if (pair.length > 1) {
